2b6fa004d4f99b1605958f555a5c6dec01bd21df
Raunak Dutta <45163382+Raunak-Dutta@users.noreply.github.com>
2019-03-20 17:10:58 +0800

    Upload 2
    upload 2

--- DebuggingExercises/DebugOne4.java
+++ DebuggingExercises/DebugOne4.java
@@ -1,8 +1,7 @@
 import javax.swing.JOptionPane;
 public class DebugOne4
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "First GUI program");
     }
 }
